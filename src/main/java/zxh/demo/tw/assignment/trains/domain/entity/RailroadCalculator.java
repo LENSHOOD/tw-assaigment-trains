4b4ec3f7df4c0ac4814ee4780bd0e53ca5fecbbc
LENSHOOD <lenshood.zxh@gmail.com>
2020-11-19 20:27:55 +0800

    feat: provide get all possible roads method

--- src/main/java/zxh/demo/tw/assignment/trains/domain/entity/RailroadCalculator.java
+++ src/main/java/zxh/demo/tw/assignment/trains/domain/entity/RailroadCalculator.java
@@ -1,14 +1,14 @@
 package zxh.demo.tw.assignment.trains.domain.entity;
 
-import static zxh.demo.tw.assignment.trains.domain.util.NullChecker.*;
+import static zxh.demo.tw.assignment.trains.domain.util.NullChecker.requireNonNull;
 
 import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.ValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
-import zxh.demo.tw.assignment.trains.domain.util.NullChecker;
 import zxh.demo.tw.assignment.trains.domain.vo.Distance;
 import zxh.demo.tw.assignment.trains.domain.vo.Station;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RailroadCalculator {
     private ValueGraph<Station, Distance> railroadGraph;
@@ -22,7 +22,39 @@ public class RailroadCalculator {
     }
 
     public List<Road> getPossibleRoads(Station startStation, Station endStation) {
-        return null;
+        ArrayList<List<Station>> finalRailroads = new ArrayList<>();
+        recursivelyFindRailroad(startStation, endStation, new ArrayList<>(List.of(startStation)), finalRailroads);
+
+        return finalRailroads.stream().map(this::toRoad).filter(Optional::isPresent).map(Optional::get).collect(Collectors.toList());
+    }
+
+    private void recursivelyFindRailroad(Station startStation, Station endStation, List<Station> preRailroad, List<List<Station>> finalRailroads) {
+        for (Station next : railroadGraph.successors(startStation)) {
+            if (preRailroad.contains(next)) {
+                continue;
+            }
+
+            ArrayList<Station> curRailroad = new ArrayList<>(preRailroad);
+            curRailroad.add(next);
+            if (!next.equals(endStation)) {
+                recursivelyFindRailroad(next, endStation, curRailroad, finalRailroads);
+            } else {
+                finalRailroads.add(curRailroad);
+            }
+        }
+    }
+
+    private Optional<Road> toRoad(List<Station> railroad) {
+        if (Objects.isNull(railroad) || railroad.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Road.RoadBuilder builder = Road.from(railroad.get(0));
+        for (int i = 1; i < railroad.size(); i++) {
+            builder.addStop(railroad.get(i), railroadGraph.edgeValue(railroad.get(i - 1), railroad.get(i)).orElse(Distance.of(0)));
+        }
+
+        return Optional.of(builder.build());
     }
 
     public static class RailroadCalculatorBuilder {
--- src/test/java/zxh/demo/tw/assignment/trains/domain/entity/RailroadCalculatorTest.java
+++ src/test/java/zxh/demo/tw/assignment/trains/domain/entity/RailroadCalculatorTest.java
@@ -1,12 +1,18 @@
 package zxh.demo.tw.assignment.trains.domain.entity;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import zxh.demo.tw.assignment.trains.domain.vo.Distance;
 import zxh.demo.tw.assignment.trains.domain.vo.Station;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class RailroadCalculatorTest {
     @Test
@@ -41,4 +47,30 @@ class RailroadCalculatorTest {
         assertThrows(NullPointerException.class, () -> builder3.addRailroad(b, c, null), "Distance cannot be null.");
     }
 
+    @Test
+    public void should_get_for_roads_from_a_to_c() {
+        // given
+        RailroadCalculator calculator = RailroadCalculator.builder()
+                .addRailroad(Station.of("A"), Station.of("B"), Distance.of(5))
+                .addRailroad(Station.of("B"), Station.of("C"), Distance.of(4))
+                .addRailroad(Station.of("C"), Station.of("D"), Distance.of(8))
+                .addRailroad(Station.of("D"), Station.of("C"), Distance.of(8))
+                .addRailroad(Station.of("D"), Station.of("E"), Distance.of(6))
+                .addRailroad(Station.of("A"), Station.of("D"), Distance.of(5))
+                .addRailroad(Station.of("C"), Station.of("E"), Distance.of(2))
+                .addRailroad(Station.of("E"), Station.of("B"), Distance.of(3))
+                .addRailroad(Station.of("A"), Station.of("E"), Distance.of(7))
+                .build();
+
+        // when
+        List<Road> possibleRoads = calculator.getPossibleRoads(Station.of("A"), Station.of("C"));
+
+        // then
+        assertThat(possibleRoads.size(), is(4));
+        List<String> roads = possibleRoads.stream().map(Road::getAllStops).map(stepList -> stepList.stream().map(Station::getName).collect(Collectors.joining())).collect(Collectors.toList());
+        assertThat(roads, hasItem("ABC"));
+        assertThat(roads, hasItem("AEBC"));
+        assertThat(roads, hasItem("ADC"));
+        assertThat(roads, hasItem("ADEBC"));
+    }
 }
