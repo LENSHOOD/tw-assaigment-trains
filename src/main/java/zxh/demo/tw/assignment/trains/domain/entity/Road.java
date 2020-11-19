2988e407cc7e16e1fa0fea7649197245ee097d06
LENSHOOD <lenshood.zxh@gmail.com>
2020-11-19 11:47:58 +0800

    feat: provide Road entity to hold a road
    Road hold the station stops like: A - B - E - F, and the whole distance summed.

--- src/main/java/zxh/demo/tw/assignment/trains/domain/entity/Road.java
+++ src/main/java/zxh/demo/tw/assignment/trains/domain/entity/Road.java
@@ -1,34 +1,80 @@
 package zxh.demo.tw.assignment.trains.domain.entity;
 
+import static java.util.Objects.*;
+
+import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.ValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
 import zxh.demo.tw.assignment.trains.domain.vo.Distance;
 import zxh.demo.tw.assignment.trains.domain.vo.Station;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Road {
-    private Station from;
-    private Station to;
-    private ValueGraph<Station, Distance> roadGraph;
+    private final Station from;
+    private final ValueGraph<Station, Distance> roadGraph;
+
+    private Road(Station from, ValueGraph<Station, Distance> roadGraph) {
+        this.from = from;
+        this.roadGraph = roadGraph;
+    }
+
+    public static RoadBuilder from(Station fromStation) {
+        return new RoadBuilder(fromStation);
+    }
 
     public List<Station> getAllStops() {
-        return null;
+        List<Station> allStops = new ArrayList<>();
+        setSuccessors(from, allStops);
+        return List.copyOf(allStops);
+    }
+
+    private void setSuccessors(Station start, List<Station> allStopsHolder) {
+        allStopsHolder.add(start);
+        roadGraph.successors(start).forEach(stop -> setSuccessors(stop, allStopsHolder));
     }
 
     public Distance getWholeDistance() {
-        return null;
+        List<Station> allStops = getAllStops();
+        int distanceValue = 0;
+        for (int i = 0; i < allStops.size(); i++) {
+            if (i == allStops.size() - 1) {
+                break;
+            }
+
+            Station currStation = allStops.get(i);
+            Station nextStation = allStops.get(i + 1);
+            distanceValue += roadGraph.edgeValue(currStation, nextStation).orElse(Distance.of(0)).getValue();
+        }
+        return Distance.of(distanceValue);
     }
 
     public static class RoadBuilder {
-        public static RoadBuilder from() {
-            return null;
+        private Station from;
+        private Station to;
+        private ImmutableValueGraph.Builder<Station, Distance> roadGraphBuilder;
+
+        private RoadBuilder(Station fromStation) {
+            from = to = requireNonNull(fromStation, "Station cannot be null.");
+            roadGraphBuilder = ValueGraphBuilder
+                    .directed()
+                    .<Station, Distance>immutable()
+                    .addNode(fromStation);
         }
 
         public RoadBuilder addStop(Station stop, Distance distance) {
-            return null;
+            roadGraphBuilder.putEdgeValue(
+                    to,
+                    requireNonNull(stop, "Station cannot be null."),
+                    requireNonNull(distance, "Distance cannot be null."));
+            to = stop;
+            return this;
         }
 
         public Road build() {
-            return null;
+            return new Road(from, roadGraphBuilder.build());
         }
     }
 }
---
+++ src/test/java/zxh/demo/tw/assignment/trains/domain/entity/RoadTest.java
@@ -0,0 +1,46 @@
+package zxh.demo.tw.assignment.trains.domain.entity;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import zxh.demo.tw.assignment.trains.domain.vo.Distance;
+import zxh.demo.tw.assignment.trains.domain.vo.Station;
+
+class RoadTest {
+    @Test
+    public void should_create_road_with_node_ABE_and_total_distance_10() {
+        Road road = Road.from(Station.of("A"))
+                .addStop(Station.of("B"), Distance.of(7))
+                .addStop(Station.of("E"), Distance.of(3))
+                .build();
+
+        assertThat(road.getAllStops(), contains(Station.of("A"), Station.of("B"), Station.of("E")));
+        assertThat(road.getWholeDistance(), is(Distance.of(10)));
+    }
+
+    @Test
+    public void should_create_single_station_road_with_total_distance_0() {
+        Road road = Road.from(Station.of("A")).build();
+
+        assertThat(road.getAllStops(), contains(Station.of("A")));
+        assertThat(road.getWholeDistance(), is(Distance.of(0)));
+    }
+
+    @Test
+    public void should_throw_Exception_if_null_station_or_distance() {
+        assertThrows(NullPointerException.class, () -> Road.from(null), "Station cannot be null.");
+
+        Road.RoadBuilder roadBuilder1 = Road.from(Station.of("A"));
+        Distance distance = Distance.of(0);
+        assertThrows(NullPointerException.class, () -> roadBuilder1.addStop(null, distance), "Station cannot be null.");
+
+        Road.RoadBuilder roadBuilder2 = Road.from(Station.of("A"));
+        Station b = Station.of("B");
+        assertThrows(NullPointerException.class, () -> roadBuilder2.addStop(b, null), "Distance cannot be null.");
+    }
+
+}
