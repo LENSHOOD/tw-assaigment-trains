804390a1c596e028e6151730c73286038d609e07
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-12-13 19:41:29 +0800

    refactor: simplify road, repalce graph to linked map

--- src/main/java/zxh/demo/tw/assignment/trains/domain/entity/Road.java
+++ src/main/java/zxh/demo/tw/assignment/trains/domain/entity/Road.java
@@ -1,23 +1,17 @@
 package zxh.demo.tw.assignment.trains.domain.entity;
 
-import static zxh.demo.tw.assignment.trains.domain.util.NullChecker.*;
+import static zxh.demo.tw.assignment.trains.domain.util.NullChecker.requireNonNull;
 
-import com.google.common.graph.ImmutableValueGraph;
-import com.google.common.graph.ValueGraph;
-import com.google.common.graph.ValueGraphBuilder;
-import zxh.demo.tw.assignment.trains.domain.util.NullChecker;
 import zxh.demo.tw.assignment.trains.domain.vo.Distance;
 import zxh.demo.tw.assignment.trains.domain.vo.Station;
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Road {
-    private final Station from;
-    private final ValueGraph<Station, Distance> roadGraph;
+    private final LinkedHashMap<Station, Distance> stationStore;
 
-    private Road(Station from, ValueGraph<Station, Distance> roadGraph) {
-        this.from = from;
-        this.roadGraph = roadGraph;
+    private Road(LinkedHashMap<Station, Distance> stationStore) {
+        this.stationStore = stationStore;
     }
 
     public static RoadBuilder from(Station fromStation) {
@@ -25,55 +19,27 @@ public class Road {
     }
 
     public List<Station> getAllStops() {
-        List<Station> allStops = new ArrayList<>();
-        setSuccessors(from, allStops);
-        return List.copyOf(allStops);
-    }
-
-    private void setSuccessors(Station start, List<Station> allStopsHolder) {
-        allStopsHolder.add(start);
-        roadGraph.successors(start).forEach(stop -> setSuccessors(stop, allStopsHolder));
+        return List.copyOf(stationStore.keySet());
     }
 
     public Distance getWholeDistance() {
-        List<Station> allStops = getAllStops();
-        int distanceValue = 0;
-        for (int i = 0; i < allStops.size(); i++) {
-            if (i == allStops.size() - 1) {
-                break;
-            }
-
-            Station currStation = allStops.get(i);
-            Station nextStation = allStops.get(i + 1);
-            distanceValue += roadGraph.edgeValue(currStation, nextStation).orElse(Distance.of(0)).getValue();
-        }
-        return Distance.of(distanceValue);
+        return Distance.of(stationStore.values().stream().mapToInt(Distance::getValue).sum());
     }
 
     public static class RoadBuilder {
-        private Station from;
-        private Station to;
-        private ImmutableValueGraph.Builder<Station, Distance> roadGraphBuilder;
+        private final LinkedHashMap<Station, Distance> stationStore = new LinkedHashMap<>();
 
         private RoadBuilder(Station fromStation) {
-            from = to = requireNonNull(fromStation);
-            roadGraphBuilder = ValueGraphBuilder
-                    .directed()
-                    .<Station, Distance>immutable()
-                    .addNode(fromStation);
+            stationStore.put(requireNonNull(fromStation), Distance.of(0));
         }
 
         public RoadBuilder addStop(Station stop, Distance distance) {
-            roadGraphBuilder.putEdgeValue(
-                    to,
-                    requireNonNull(stop),
-                    requireNonNull(distance));
-            to = stop;
+            stationStore.put(requireNonNull(stop), requireNonNull(distance));
             return this;
         }
 
         public Road build() {
-            return new Road(from, roadGraphBuilder.build());
+            return new Road(stationStore);
         }
     }
 }
